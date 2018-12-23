0dd0010c5e659280d507c67fe4eff5198275fa36
Sebastian Camacho <sxcamacho@gmail.com>
2018-12-23 01:56:34 -0300

    update app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HomeViewComponent } from './views/home-view/home-view.component'
+import { SearchBoxComponent } from './components/search-box/search-box.component'
+import { PaginationComponent } from './components/pagination/pagination.component'
+import { UserListComponent } from './components/user-list/user-list.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    SearchBoxComponent,
+    PaginationComponent,
+    UserListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
